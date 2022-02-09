640bcf9ea988196cafe034f6348d8a9c869925f1
Татьяна Анашкина <93616371+anashkina4050@users.noreply.github.com>
2022-02-09 23:11:10 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "5174364274:AAEW5idUpbJ39BnD6LNwbTHnbT-nlFuVlag"
+
+keys = {
+    "доллар": "USD",
+    "евро": "EUR",
+    "биткоин": "BTC",
+    "юань": "CNY",
+    "гривна": "UAH",
+    "рубль": "RUB"
+}
